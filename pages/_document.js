8755faf5d337bd3154a8f74a5f80e0778b5aa143
NGX <minhkhangsolo@gmail.com>
2023-05-14 18:03:11 +0700

    code o

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
+          <title>
+             CLB Bóng Bàn
+          </title>
         </Head>
         <body>
           <Main />
